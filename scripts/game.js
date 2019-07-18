faedc6d20f24a96329b123ce4dc7a1fd1b6c516b
Jackie Symons <jackiedsymons@gmail.com>
2019-07-17 21:06:12 -0400

    remove jquery

--- scripts/game.js
+++ scripts/game.js
@@ -192,13 +192,4 @@ function safeSquares () {
 };
 
 
-
-if($(window).width() <= 600){
-  $('.rules-open').removeClass('hide');
-  $('.game-rules-body').addClass('hide');
-  $('.rules-close').addClass('hide');
-}
-
-
-
 });
